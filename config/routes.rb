abd29fb9727c64632df76852deca4c4239e59914
murat atalay <atalaymurat@msn.com>
2019-11-18 18:19:31 +0300

    user registrations update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:registrations]
+  as :user do
+      get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+      patch 'users' => 'devise/registrations#update', :as => 'user_registration'            
+  end
   resources :sales
   resources :people
   resources :companies

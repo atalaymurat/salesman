6cc19bb92a826463a412767f43bb688f1155c8c1
murat atalay <atalaymurat@msn.com>
2019-11-18 17:52:30 +0300

    not registrable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable and :registerable,
+
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
   def name
     self.email.split("@").first
   end

e8eccbbfee61fe6abdb226c51f08f9a7e34cc12a
murat atalay <atalaymurat@msn.com>
2019-12-14 15:27:20 +0300

    new commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   mount_uploaders :pictures, ImageUploader
+  before_create :auto_code
+
+  def auto_code
+    new_code = "P" + Time.now.strftime('%y').to_s + (Product.last.id + 1).to_s
+      self.item_code = new_code
+  end
 end

846082c4617a80a1325b1a39215c8b56dd0b7088
atalaymurat <atalaymurat@msn.com>
2020-03-19 13:17:59 +0300

    sales helper

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -3,6 +3,7 @@ module SalesHelper
        
       { "Nakit Ödeme" =>  "Nakit ödeme olarak fiyatlandırılmıştır. - Cash payment before shipment" , 
         "%20 siparişte" => "%20 siparişte, %80 bakiyesi makina fabrikadan sevkiyata hazır olduğunda - T/T 20% advance payment with order, 80% on advice goods ready for delivery",
+        "%30 siparişte" => "%30 siparişte, %70 bakiyesi makina fabrikadan sevkiyata hazır olduğunda - T/T 20% advance payment with order, 80% on advice goods ready for delivery",
       "Karşılıklı Görüşme" => "Karşılıklı görüşmeler neticesinde belirlenecektir - Will be clearify after negotiations",
         "Yüklemeden Önce" => "%20 siparişte, %80 bakiyesi makina sevkiyata hazır olduğunda - 20% advance payment with order, 80% on advice goods ready for delivery",
         "Vadeli" => "Belirlenen vadelerde ödenecektir",
@@ -12,9 +13,11 @@ module SalesHelper
   end
   def delivery_condition_hash
     {
-      "Ex-Works" => "İstanbul-Türkiye Satıcı Depolarından teslim - EXW Istanbul/TURKEY incoterms 2010",
-      "CIF-ISTANBUL" => "CIF-ISTANBUL Gümrüklerinden alıcıya devir edilecektir - CIF-ISTANBUL",
-      "DAP buyer loc" => "Alıcı adresine araç üzerinde teslim edilecektir - DAP buyer location"
+      "Ex-Works Varol" => "İstanbul-Türkiye Satıcı Depolarından teslim - EXW Istanbul/TURKEY incoterms 2010",
+      "Ex-Works Homag" => "Homag Almanya fabrikadan teslim edilecektir- EXW EX-WORKS incoterms 2010",
+      "CIF-ISTANBUL" => "CIF-ISTANBUL Gümrüklerinden alıcıya devir edilecektir - CIF-ISTANBUL incoterms 2010",
+      "CIF-ANKARA" => "CIF-ANKARA Gümrüklerinden alıcıya devir edilecektir - CIF-ANKARA incoterms 2010",
+      "DAP ALICI FABRIKA" => "Alıcı adresine araç üzerinde teslim edilecektir - DAP buyer location"
     }
   end
 end

a737a905dcb6d94433ccc04d15be5e46f99ad37d
mAta <atalaymurat@msn.com>
2020-07-08 15:33:16 +0300

    company sales

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ApplicationRecord
   has_many :emails, dependent: :destroy
-  has_many :companies
+  has_many :sales
   accepts_nested_attributes_for :emails, allow_destroy: true, reject_if: proc { |attributes|   attributes['email'].blank? }
 end

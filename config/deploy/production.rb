bc3567cc1a0bb8cdb15e8a30ad2c0efb1a284c2e
mAta <atalaymurat@msn.com>
2020-12-08 11:08:33 +0300

    chg srv

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '35.242.251.230', user: 'atalaymurat', roles: %w{app db web}
+server '185.145.97.156', user: 'atalaymurat', roles: %w{app db web}
 
 
 
